73cb642ac90d244e95d1c33e0738f8c5a6843568
Victor Hugo <vhaporfiro@gmail.com>
2025-05-25 11:43:53 -0300

    Criação do projeto, frontend (Angular), backend(Springboot), configuração do banco de dados e criação do primeiro script do banco de dados.

---
+++ Clinica-Portal-Do-Autista-backend/src/main/java/com/clinicaportaldoautista/ClinicaPortalDoAutistaApplication.java
@@ -0,0 +1,13 @@
+package com.clinicaportaldoautista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaPortalDoAutistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaPortalDoAutistaApplication.class, args);
+	}
+
+}
---
+++ Clinica-Portal-Do-Autista-backend/src/test/java/com/clinicaportaldoautista/ClinicaPortalDoAutistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicaportaldoautista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaPortalDoAutistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
